644c825dc95c04ed16cd1af099a566e4d813e191
Ben Blackman <bblkmn5@gmail.com>
2017-10-29 23:13:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
   def self.create(name)
     Song.new(name).tap{ |s| s.save}
   end
-
+#finds a song instance in @@all by the name property of the song
   def self.find_by_name(name)
     all.find{|song| song.name == name}
   end
