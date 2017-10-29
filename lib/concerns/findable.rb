79a4f8064869dbee2d676dd7c7c69a7089b6ab9b
Ben Blackman <bblkmn5@gmail.com>
2017-10-29 23:16:34 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,7 +2,7 @@ module Concerns::Findable
 #is added as a class method to classes that extend the module
     def find_by_name(name)
 #searching the extended class's @@all variable for an instance that matches the provided name
-      @@all.find{ |instance| instance.name == name}
+      all.find{ |instance| instance.name == name}
     end
 
     def find_or_create_by_name(name)
